a3f2edb5ea2ad3b2e8c644eb063b7d880d216ed0
Techwebcoder <rajakumar.xia.11117@gmail.com>
2026-04-25 20:55:20 +0530

    basic hasing using the bcrypt and implement json web token and it is send to the user beginning

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
+var jwt = require('jsonwebtoken');
 
-// ROUTE 1: Create a user using POST "/api/auth"
+const JWT_SECRET = "Rajakumarisagoodboy$"
+
+// ROUTE 1: Create a user using POST "/api/auth/createuser"
 router.post(
-  '/',
+  '/createuser',
   [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
@@ -25,14 +29,23 @@ router.post(
         return res.status(400).json({ error: 'Sorry, a user with this email already exists' });
       }
 
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password,salt);
       // create new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: secPass
       });
 
-      res.json(user);
+      const data = {
+        user : {
+          id : user.id
+        }
+      }
+      const authtoken = jwt.sign(data , JWT_SECRET);
+      
+      res.json({authtoken});
     } catch (error) {
       console.error(error.message);
       res.status(500).send('Internal Server Error');
